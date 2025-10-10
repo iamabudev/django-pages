1990d133a669776c7dee4a5e9dbf6d08c0957591
iamabudev <ergashboyevabdulloh0@gmail.com>
2025-10-10 11:27:13 +0800

    Initial commit

---
+++ pagesapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+
+class LocationsPageView(TemplateView):
+    template_name = 'Location.html'
+
+
